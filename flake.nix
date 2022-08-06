9e6d139f122f2a3013f032130f360b16126068ce
Rafael Fernández López <ereslibre@ereslibre.es>
2022-08-07 01:32:38 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -29,8 +29,9 @@
         nixosConfigurations = {
           "cpi-5.lab.ereslibre.local" = nixpkgs-rpi.lib.nixosSystem {
             system = "aarch64-linux";
-            modules = [ ./cpi-5/configuration.nix ];
-            dotfiles.home-manager.nixosModules.home-manager
+            modules = [
+              ./cpi-5/configuration.nix
+              dotfiles.home-manager.nixosModules.home-manager
               {
                 home-manager.users.ereslibre =
                   ((import "${dotfiles}/hm-configurations.nix" {
@@ -39,6 +40,7 @@
                     stateVersion = "22.05";
                   })."ereslibre@cpi-5.lab.ereslibre.local".configuration);
               }
+            ];
           };
           "nuc-1.lab.ereslibre.local" = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
