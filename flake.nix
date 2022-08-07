0d5d3eba08ea9051fd6e43611958103902d57523
Rafael Fernández López <ereslibre@ereslibre.es>
2022-08-07 10:25:28 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -3,20 +3,13 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
-    # nixpkgs revision known to work with Raspberry Pi 4b/400
-    nixpkgs-rpi.url =
-      "github:nixos/nixpkgs/c71f061c68ba8ce53471b767d5049cbd0f3d8490";
-    dotfiles = {
-      url = "github:ereslibre/dotfiles";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    dotfiles.url = "github:ereslibre/dotfiles";
   };
 
-  outputs = { flake-utils, nixpkgs, nixpkgs-rpi, dotfiles, ... }:
+  outputs = { flake-utils, dotfiles, ... }:
     flake-utils.lib.eachSystem
     (flake-utils.lib.defaultSystems ++ [ "aarch64-darwin" ]) (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let pkgs = dotfiles.nixpkgs.legacyPackages.${system};
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [ cachix nix-linter nixfmt ];
@@ -24,25 +17,25 @@
       }) // {
         # Make Home Manager configurations available from here for
         # applying locally as well if desired.
-        inherit (dotfiles) homeConfigurations;
+        inherit (dotfiles) homeConfigurations homeConfigurationsRpi;
 
         nixosConfigurations = {
-          "cpi-5.lab.ereslibre.local" = nixpkgs-rpi.lib.nixosSystem {
+          "cpi-5.lab.ereslibre.local" = dotfiles.nixpkgs-rpi.lib.nixosSystem {
             system = "aarch64-linux";
             modules = [
               ./cpi-5/configuration.nix
-              dotfiles.home-manager.nixosModules.home-manager
+              dotfiles.home-manager-rpi.nixosModules.home-manager
               {
                 home-manager.users.ereslibre =
                   ((import "${dotfiles}/hm-configurations.nix" {
-                    inherit (dotfiles) home-manager;
-                    nixpkgs = nixpkgs-rpi;
+                    home-manager = dotfiles.home-manager-rpi;
+                    nixpkgs = dotfiles.nixpkgs-rpi;
                     stateVersion = "22.05";
                   })."ereslibre@cpi-5.lab.ereslibre.local".configuration);
               }
             ];
           };
-          "nuc-1.lab.ereslibre.local" = nixpkgs.lib.nixosSystem {
+          "nuc-1.lab.ereslibre.local" = dotfiles.nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
               ./nuc-1/configuration.nix
@@ -50,8 +43,7 @@
               {
                 home-manager.users.ereslibre =
                   ((import "${dotfiles}/hm-configurations.nix" {
-                    inherit (dotfiles) home-manager;
-                    inherit nixpkgs;
+                    inherit (dotfiles) home-manager nixpkgs;
                     stateVersion = "22.05";
                   })."ereslibre@nuc-1.lab.ereslibre.local".configuration);
               }
