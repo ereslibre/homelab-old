05d7ffd438b2ad2a1bdfc48c761ae0f00a899f88
Rafael Fernández López <ereslibre@ereslibre.es>
2022-08-15 12:46:04 +0200

    Add pidesktop

--- pidesktop/configuration.nix
+++ pidesktop/configuration.nix
@@ -5,7 +5,10 @@
     ./hardware-configuration.nix
     "${
       fetchTarball
-      "https://github.com/NixOS/nixos-hardware/archive/936e4649098d6a5e0762058cb7687be1b2d90550.tar.gz"
+      {
+        url = "https://github.com/NixOS/nixos-hardware/archive/936e4649098d6a5e0762058cb7687be1b2d90550.tar.gz";
+        sha256 = "sha256:06g0061xm48i5w7gz5sm5x5ps6cnipqv1m483f8i9mmhlz77hvlw";
+      }
     }/raspberry-pi/4"
   ];
 
