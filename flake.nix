686e35d733d1bf8f625c800c362dffb10e71fd08
Rafael Fernández López <ereslibre@ereslibre.es>
2022-12-16 00:02:06 +0100

    Update

--- flake.nix
+++ flake.nix
@@ -18,36 +18,34 @@
       }) // {
         # Make Home Manager configurations available from here for
         # applying locally as well if desired.
-        inherit (dotfiles) homeConfigurations homeConfigurationsRpi;
+        inherit (dotfiles) homeConfigurations;
 
         nixosConfigurations = {
           "pi-office.lab.ereslibre.local" =
-            dotfiles.nixpkgs-rpi.lib.nixosSystem {
+            dotfiles.nixpkgs.lib.nixosSystem {
               system = "aarch64-linux";
               modules = [
                 ./pi-office/configuration.nix
-                dotfiles.home-manager-rpi.nixosModules.home-manager
+                dotfiles.home-manager.nixosModules.home-manager
                 {
                   home-manager.users.ereslibre =
                     ((import "${dotfiles}/hm-configurations.nix" {
-                      home-manager = dotfiles.home-manager-rpi;
-                      nixpkgs = dotfiles.nixpkgs-rpi;
+                      inherit (dotfiles) home-manager nixpkgs;
                     })."ereslibre@pi-office.lab.ereslibre.local".configuration);
                 }
               ];
             };
           "pi-desktop.lab.ereslibre.local" =
-            dotfiles.nixpkgs-rpi.lib.nixosSystem {
+            dotfiles.nixpkgs.lib.nixosSystem {
               system = "aarch64-linux";
               modules = [
                 ./pi-desktop/configuration.nix
                 nixos-hardware.nixosModules.raspberry-pi-4
-                dotfiles.home-manager-rpi.nixosModules.home-manager
+                dotfiles.home-manager.nixosModules.home-manager
                 {
                   home-manager.users.ereslibre =
                     ((import "${dotfiles}/hm-configurations.nix" {
-                      home-manager = dotfiles.home-manager-rpi;
-                      nixpkgs = dotfiles.nixpkgs-rpi;
+                      inherit (dotfiles) home-manager nixpkgs;
                     })."ereslibre@pi-desktop.lab.ereslibre.local".configuration);
                 }
               ];
