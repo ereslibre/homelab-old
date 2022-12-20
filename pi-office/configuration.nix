ce8ab1c769576e7411dc372b1d29227b81b6fdfb
Rafael Fernández López <ereslibre@ereslibre.es>
2022-12-20 18:34:15 +0100

    Update

--- pi-office/configuration.nix
+++ pi-office/configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix ];
+  imports = [ ./hardware-configuration.nix ../common/linux-node/podman.nix ];
 
   nix.extraOptions = ''
     experimental-features = nix-command flakes
