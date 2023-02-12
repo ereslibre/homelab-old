0b37c9045ce33d0700db4e197b6068816115479f
Rafael Fernández López <ereslibre@ereslibre.es>
2023-02-12 20:28:04 +0100

    Update

--- pi-desktop/configuration.nix
+++ pi-desktop/configuration.nix
@@ -14,11 +14,6 @@
     };
   };
 
-  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
-  boot.loader.grub.enable = false;
-  # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
-
   environment.systemPackages = with pkgs; [ man-pages man-pages-posix ];
 
   time.timeZone = "Europe/Madrid";
