4e11f69238097ec906c08a314b40585b038c90dc
Rafael Fernández López <ereslibre@ereslibre.es>
2023-04-04 19:04:30 +0200

    Update

--- pi-office/hardware-configuration.nix
+++ pi-office/hardware-configuration.nix
@@ -3,7 +3,7 @@
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
