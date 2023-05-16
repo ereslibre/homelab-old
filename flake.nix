42c37515ee3cace392796fd05acd93b89616445e
Rafael Fernández López <ereslibre@ereslibre.es>
2023-05-16 23:52:03 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -22,30 +22,28 @@
     })
     // (let
       mapMachineConfigurations = configurations:
-        dotfiles.nixpkgs.lib.attrsets.mapAttrs (name: configuration:
+        dotfiles.nixpkgs.lib.attrsets.mapAttrs (host: configuration:
           dotfiles.nixpkgs.lib.nixosSystem {
             inherit (configuration) system;
             modules =
               configuration.modules
               ++ (let
-                machineConfig =
+                hmConfig = user: host:
                   (import "${dotfiles}/hm-configurations.nix" {
                     inherit (dotfiles) devenv home-manager nixpkgs;
                   })
-                  ."ereslibre@nuc-2" # FIXME
+                  ."${user}@${host}"
                   .rawConfig;
               in [
                 dotfiles.home-manager.nixosModules.home-manager
                 {
                   home-manager.extraSpecialArgs =
-                    machineConfig.extraSpecialArgs;
-                  home-manager.users.ereslibre = {
-                    config,
-                    pkgs,
-                    ...
-                  }: {
-                    imports = machineConfig.modules;
-                  };
+                    (hmConfig "ereslibre" "nuc-2").extraSpecialArgs; # FIXME
+                  home-manager.users = builtins.listToAttrs (map (user: {
+                      name = user;
+                      value = {...}: {imports = (hmConfig user host).modules;};
+                    })
+                    configuration.users);
                 }
               ]);
           })
@@ -54,6 +52,7 @@
       nixosConfigurations = mapMachineConfigurations {
         "pi-office" = {
           system = "aarch64-linux";
+          users = ["ereslibre"];
           modules = [
             ./pi-office/configuration.nix
             nixos-hardware.nixosModules.raspberry-pi-4
@@ -61,6 +60,7 @@
         };
         "pi-desktop" = {
           system = "aarch64-linux";
+          users = ["ereslibre"];
           modules = [
             ./pi-desktop/configuration.nix
             nixos-hardware.nixosModules.raspberry-pi-4
@@ -68,10 +68,12 @@
         };
         "nuc-1" = {
           system = "x86_64-linux";
+          users = ["ereslibre"];
           modules = [./nuc-1/configuration.nix];
         };
         "nuc-2" = {
           system = "x86_64-linux";
+          users = ["ereslibre"];
           modules = [./nuc-2/configuration.nix];
         };
       };
