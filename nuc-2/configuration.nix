a75978f60b5fbac66c7f6272e5e71272a2980da3
Rafael Fernández López <ereslibre@ereslibre.es>
2023-05-20 11:03:20 +0200

    Set irqpoll still; kernel error still shows up in logs

--- nuc-2/configuration.nix
+++ nuc-2/configuration.nix
@@ -7,6 +7,9 @@
       efi.canTouchEfiVariables = true;
     };
     kernelParams = [
+      # On this machine, at certain time after startup, dmesg says:
+      #   [  144.383019] irq 16: nobody cared (try booting with the "irqpoll" option)
+      "irqpoll"
       "nohibernate"
     ];
   };
