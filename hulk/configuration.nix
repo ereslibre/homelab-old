931c5bf12c51db4e5b3ebd4d0f65609cbe8866ac
Rafael Fernández López <ereslibre@ereslibre.es>
2023-11-21 09:52:34 +0100

    Update

--- hulk/configuration.nix
+++ hulk/configuration.nix
@@ -17,6 +17,8 @@
 
   networking.hostName = "hulk";
 
+  virtualisation.podman.enableNvidia = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
