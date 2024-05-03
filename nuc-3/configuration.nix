eab0855dd434e38f0e95ed50a97a5514a4be91ee
Rafael Fernández López <ereslibre@ereslibre.es>
2024-05-03 17:18:37 +0200

    Update

--- nuc-3/configuration.nix
+++ nuc-3/configuration.nix
@@ -17,6 +17,8 @@
 
   networking.hostName = "nuc-3";
 
+  users.users.ereslibre.extraGroups = ["video"];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
