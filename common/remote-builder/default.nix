4efe025b008d095025a1309fa15e064efc461890
Rafael Fernández López <ereslibre@ereslibre.es>
2024-05-06 14:29:47 +0200

    Update

--- common/remote-builder/default.nix
+++ common/remote-builder/default.nix
@@ -1,9 +1,14 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   users.groups.builder = {};
   users.users.builder = {
     isSystemUser = true;
     extraGroups = ["wheel"];
     group = "builder";
+    shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPgXdZGKpuMlgyDqjUt38Yb0fdkEqMWhSdWKvzFDJG4M"
     ];
