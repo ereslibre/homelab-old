e175d5edeede4626ab84e7c0769d8e09925b1a16
Rafael Fernández López <ereslibre@ereslibre.es>
2024-06-12 08:57:00 +0200

    Update

--- rafaels-air/configuration.nix
+++ rafaels-air/configuration.nix
@@ -9,6 +9,16 @@
     home = "/Users/ereslibre";
     shell = pkgs.zsh;
   };
+  networking.knownNetworkServices = [
+    "Dell Universal Dock D6000"
+    "USB 10/100/1000 LAN"
+    "VM network interface"
+    "USB 10/100 LAN"
+    "Dell Universal Hybrid Video Doc"
+    "Thunderbolt Bridge"
+    "Wi-Fi"
+    "Tailscale Tunnel"
+  ];
   nix.gc.automatic = true;
   services = {
     karabiner-elements.enable = true;
