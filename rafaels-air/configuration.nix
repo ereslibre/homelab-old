00e7f824682580f2a05507722f8d5d73481effd0
Rafael Fernández López <ereslibre@ereslibre.es>
2024-07-16 20:40:14 +0200

    Update

--- rafaels-air/configuration.nix
+++ rafaels-air/configuration.nix
@@ -4,7 +4,7 @@
     systemPackages = with pkgs; [tailscale zsh];
     systemPath = ["/run/current-system/sw/bin"];
   };
-  fonts.packages = with pkgs; [ fira-code ];
+  fonts.packages = with pkgs; [fira-code];
   users.users.ereslibre = {
     createHome = true;
     home = "/Users/ereslibre";
